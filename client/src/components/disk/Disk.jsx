55de1be0a3a55529faf8c4401865c27b7089b2f1
Шевченко Максим <shevchenko@gogpb.ru>
2021-01-31 19:31:23 +0200

    added back button and func

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,15 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getFiles, uploadFile} from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './disk.scss'
 import Popup from './Popup';
-import {setPopupDisplay, setCurrentDir} from "../../reducers/fileReducer";
+import {setPopupDisplay, setCurrentDir, popFromStack} from "../../reducers/fileReducer";
 
 const Disk = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
     const lastDir = useSelector(state => state.files.lastDir)
+    const [dragEnter, setDragEnter] = useState(false)
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
@@ -21,6 +22,7 @@ const Disk = () => {
 
     const backClickHandler = () => {
       dispatch(setCurrentDir(lastDir))
+      dispatch(popFromStack())
     }
 
     function fileUploadHandler(event) {
@@ -30,9 +32,32 @@ const Disk = () => {
       files.forEach(file => dispatch(uploadFile(file, currentDir)))
     }
 
+    function dragEnterHandler(event) {
+      event.preventDefault()
+      event.stopPropagation()
+      console.log('dragEnter')
+      setDragEnter(true)
+    }
 
-    return (
-        <div className="disk">
+    function dragLeaveHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        console.log('dragLeave')
+        setDragEnter(false)
+    }
+
+    function dropHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        console.log('drop')
+        let files = [...event.dataTransfer.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+        setDragEnter(false)
+    }
+
+
+    return ( !dragEnter ?
+        <div className="disk" onDragEnter={dragEnterHandler} onDragLeave={() => dragLeaveHandler()} onDragOver={dragEnterHandler}>
             <div className="disk__btns">
                 {currentDir && <button className="disk__back" onClick={()=> backClickHandler()}>Back</button>}
                 <button className="disk__create" onClick={() => showPopupHandler()}>Создать папку</button>
@@ -44,7 +69,11 @@ const Disk = () => {
             <FileList/>
             <Popup/>
         </div>
-    );
-};
+        :
+        <div className="drop-area" onDrop={dropHandler} onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
+            Перетащите файлы сюда
+        </div>
+    )
+}
 
 export default Disk;
--- client/src/reducers/fileReducer.js
+++ client/src/reducers/fileReducer.js
@@ -19,16 +19,24 @@ export default function fileReducer(state = defaultState, action) {
         case SET_CURRENT_DIR: return {...state, currentDir: action.payload}
         case ADD_FILE: return {...state, files: [...state.files, action.payload]}
         case SET_POPUP_DISPLAY: return {...state, popupDisplay: action.payload}
-        case PUSH_TO_STACK: return {...state, dirStack: [...state.dirStack, action.payload]}
-        case POP_FROM_STACK: return {...state, lastDir: state.dirStack.pop()}
+        case PUSH_TO_STACK:
+            const updatedDir = [...state.dirStack, action.payload]
+            let updatedLastDir = updatedDir.slice(-1)[0]
+            return {...state, lastDir:updatedLastDir, dirStack: updatedDir }
+        case POP_FROM_STACK:
+            const newDir = state.dirStack.filter(item => item !== state.dirStack.slice(-1)[0])
+            let newLastDir = newDir.slice(-1)[0]
+            return {...state, lastDir: newLastDir, dirStack: newDir}
         default:
             return state
     }
 }
+
+// ДОДЕЛАТЬ ФУНКЦИОНАЛ КНОПКИ НАЗАД ПО ПАПКАМ popFromStack/lastDir
 
 export const setFiles = (files) => ({type: SET_FILES, payload: files})
 export const setCurrentDir= (dir) => ({type: SET_CURRENT_DIR, payload: dir})
 export const addFile = (file) => ({type: ADD_FILE, payload: file})
 export const setPopupDisplay = (display) => ({type: SET_POPUP_DISPLAY, payload: display})
 export const pushToStack = (dir) => ({type: PUSH_TO_STACK, payload: dir})
-export const popFromStack = (dir) => ({type: POP_FROM_STACK})
+export const popFromStack = () => ({type: POP_FROM_STACK})
